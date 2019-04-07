9b7a76ea79e22a766882a5fb4c2862b2cd9dbbd8
Simon Harvan <simon.harvan@pixwell.sk>
2019-04-07 18:38:43 +0200

    Indentation fixed

--- counting-people.ino
+++ counting-people.ino
@@ -59,196 +59,204 @@ ESP8266WiFiMulti WiFiMulti;
 
 void setup()
 {
-  delay(1000);
-  Wire.begin(0, 4);
-  Wire.setClock(400000);
+	delay(1000);
+	Wire.begin(0, 4);
+	Wire.setClock(400000);
 
-  Serial.begin(115200);
-  while (!Serial); //Wait for user to open terminal
-  
-  if (isConnected() == false)
-  {
-    Serial.println("MLX90640 not detected at default I2C address. Please check wiring. Freezing.");
-    while (1);
-  }
+	Serial.begin(115200);
+	while (!Serial); //Wait for user to open terminal
 
-  //Get device parameters - We only have to do this once
-  int status;
-  uint16_t eeMLX90640[832];
-  
-  status = MLX90640_DumpEE(MLX90640_address, eeMLX90640);
-  if (status != 0)
-    Serial.println("Failed to load system parameters");
+	if (isConnected() == false)
+	{
+		Serial.println("MLX90640 not detected at default I2C address. Please check wiring. Freezing.");
+		while (1);
+	}
 
-  status = MLX90640_ExtractParameters(eeMLX90640, &mlx90640);
-  if (status != 0)
-    Serial.println("Parameter extraction failed");
+	//Get device parameters - We only have to do this once
+	int status;
+	uint16_t eeMLX90640[832];
 
-  //Once params are extracted, we can release eeMLX90640 array
-  MLX90640_SetRefreshRate(MLX90640_address, 0x05); //Set rate to 8Hz
+	status = MLX90640_DumpEE(MLX90640_address, eeMLX90640);
+	if (status != 0)
+		Serial.println("Failed to load system parameters");
 
-  for (uint8_t t = 4; t > 0; t--) {
-    Serial.printf("[SETUP] WAIT %d...\n", t);
-    Serial.flush();
-    delay(1000);
-  }
+	status = MLX90640_ExtractParameters(eeMLX90640, &mlx90640);
+	if (status != 0)
+		Serial.println("Parameter extraction failed");
 
-  WiFi.mode(WIFI_STA);
-  WiFiMulti.addAP("Muhaha", "woma2125");
-  Wire.setClock(1000000);
+	//Once params are extracted, we can release eeMLX90640 array
+	MLX90640_SetRefreshRate(MLX90640_address, 0x05); //Set rate to 8Hz
+
+	for (uint8_t t = 4; t > 0; t--) {
+		Serial.printf("[SETUP] WAIT %d...\n", t);
+		Serial.flush();
+		delay(1000);
+	}
+
+	WiFi.mode(WIFI_STA);
+	WiFiMulti.addAP("Muhaha", "woma2125");
+	Wire.setClock(1000000);
 }
 
 
 void loop()
 {
-  long startTime = millis();
-  long stopTime;
-  for (byte x = 0 ; x < 2 ; x++)
-  {
-    uint16_t mlx90640Frame[834];
-    int status = MLX90640_GetFrameData(MLX90640_address, mlx90640Frame);
-    stopTime = millis();
-    if (status < 0)
-    {
-      Serial.print("GetFrame Error: ");
-      Serial.println(status);
-    }
 
-    float vdd = MLX90640_GetVdd(mlx90640Frame, &mlx90640);
-    float Ta = MLX90640_GetTa(mlx90640Frame, &mlx90640);
+	long startTime = millis();
+
+	uint16_t mlx90640Frame[834];
+
+	int status = getFrameData(MLX90640_address, mlx90640Frame);
+	if (status < 0)
+	{
+		Serial.print("GetFrame Error: ");
+		Serial.println(status);
+	}
+	float vdd = MLX90640_GetVdd(mlx90640Frame, &mlx90640);
+	float Ta = MLX90640_GetTa(mlx90640Frame, &mlx90640);
 
     float tr = Ta - TA_SHIFT; //Reflected temperature based on the sensor ambient temperature
     float emissivity = 0.95;
 
     MLX90640_CalculateTo(mlx90640Frame, &mlx90640, emissivity, tr, mlx90640To);
-  }
-  
 
-  Serial.print("Read rate: ");
-  Serial.print(stopTime - startTime);
-  Serial.println(" ms");
+    long stopTime = millis();
 
-  memset(textToSend, 0, sizeof(textToSend));
-  strcat(textToSend, "{\"text\":\"");
- 
-  char tmp[7];
- 
-  for (int x = 0 ; x < 768 ; x++)
-  {
-    Serial.print(mlx90640To[x], 2);
-    Serial.print(",");
-    memset(tmp, 0, sizeof(tmp));
-    ftoa(mlx90640To[x], tmp, 2);
-    
-    strcat(tmp, ",");
-    strcat(textToSend, tmp);
-  }
-  strcat(textToSend, "\"}");
-  
-  Serial.println("");
-  
+
+    Serial.print("Read rate: ");
+    Serial.print(stopTime - startTime);
+    Serial.println(" ms");
+
+    memset(textToSend, 0, sizeof(textToSend));
+    strcat(textToSend, "{\"text\":\"");
+
+    char tmp[7];
+
+    for (int x = 0 ; x < 768 ; x++)
+    {
+    	Serial.print(mlx90640To[x], 2);
+    	Serial.print(",");
+    	memset(tmp, 0, sizeof(tmp));
+    	ftoa(mlx90640To[x], tmp, 2);
+
+    	strcat(tmp, ",");
+    	strcat(textToSend, tmp);
+    }
+    strcat(textToSend, "\"}");
+
+    Serial.println("");
+
 //  sendData(textToSend);
-  
-  
-//  counter++;
 }
 
+int getFrameData(uint8_t slaveAddr, uint16_t *frameData) {
+  	uint16_t dataReady = 1;
+  	uint16_t controlRegister1;
+  	uint16_t statusRegister;
+  	int error = 1;
+  	uint8_t cnt = 0;
+
+  	dataReady = 0;
+  	while(dataReady == 0)
+  	{
+  		error = MLX90640_I2CRead(slaveAddr, 0x8000, 1, &statusRegister);
+  		if(error != 0)
+  		{
+  			return error;
+  		}    
+  		dataReady = statusRegister & 0x0008;
+  	}   
+  }
+
 //Returns true if the MLX90640 is detected on the I2C bus
-boolean isConnected()
-{
-  
-  Wire.beginTransmission((uint8_t)MLX90640_address);
-  if (Wire.endTransmission() != 0)
-    return (false); //Sensor did not ACK
-  return (true);
+boolean isConnected() {
+
+  	Wire.beginTransmission((uint8_t)MLX90640_address);
+  	if (Wire.endTransmission() != 0)
+    	return (false); //Sensor did not ACK
+  	return (true);
 }
 
 void sendData(char* data) {
-  if ((WiFiMulti.run() == WL_CONNECTED)) {
+	if ((WiFiMulti.run() == WL_CONNECTED)) {
 
-      WiFiClient client;
-    
-      HTTPClient http;
-  
-      Serial.print("[HTTP] begin...\n");
-      if (http.begin(client, "http://147.175.121.212/api/logs")) {  // HTTP
-        
-        http.addHeader("Content-Type", "application/json");
-        http.addHeader("x-access-token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1NTQ0NTI1NTEsImV4cCI6MTU1NDUzODk1MX0.wJrx4-i_y9v8bCfrPYoYrGNmSKctTABE_5MmgKunwfQ");
-        Serial.print("[HTTP] POST...\n");
-        // start connection and send HTTP header
-        
-        int httpCode = http.POST(data);
-        
-        // httpCode will be negative on error
-        if (httpCode > 0) {
-          // HTTP header has been send and Server response header has been handled
-          Serial.printf("[HTTP] GET... code: %d\n", httpCode);
-  
-          // file found at server
-          if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
-            String payload = http.getString();
-            Serial.println(payload);
-          }
-        } else {
-          Serial.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
-        }
+		WiFiClient client;
+
+		HTTPClient http;
+
+		Serial.print("[HTTP] begin...\n");
+		if (http.begin(client, "http://147.175.121.212/api/logs")) {  // HTTP
+
+			http.addHeader("Content-Type", "application/json");
+			http.addHeader("x-access-token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1NTQ0NTI1NTEsImV4cCI6MTU1NDUzODk1MX0.wJrx4-i_y9v8bCfrPYoYrGNmSKctTABE_5MmgKunwfQ");
+			Serial.print("[HTTP] POST...\n");
+			
+			// start connection and send HTTP header
+			int httpCode = http.POST(data);
+
+			// httpCode will be negative on error
+			if (httpCode > 0) {
+		  		// HTTP header has been send and Server response header has been handled
+				Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+
+		  		// file found at server
+				if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+					String payload = http.getString();
+					Serial.println(payload);
+				}
+			} else {
+				Serial.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
+			}
+		}
     }
-  }
 }
 
-void reverse(char *str, int len) 
-{ 
-    int i=0, j=len-1, temp; 
-    while (i<j) 
-    { 
-        temp = str[i]; 
-        str[i] = str[j]; 
-        str[j] = temp; 
-        i++; j--; 
-    } 
+void reverse(char *str, int len) { 
+	int i=0, j=len-1, temp; 
+	while (i<j) { 
+		temp = str[i]; 
+		str[i] = str[j]; 
+		str[j] = temp; 
+		i++; j--; 
+	} 
 } 
 
-int intToStr(int x, char str[], int d) 
-{ 
-    int i = 0; 
-    while (x) 
-    { 
-        str[i++] = (x%10) + '0'; 
-        x = x/10; 
-    } 
-  
-    // If number of digits required is more, then 
-    // add 0s at the beginning 
-    while (i < d) 
-        str[i++] = '0'; 
-  
-    reverse(str, i); 
-    str[i] = '\0'; 
-    return i; 
+int intToStr(int x, char str[], int d) { 
+	int i = 0; 
+	while (x) { 
+		str[i++] = (x%10) + '0'; 
+		x = x/10; 
+	} 
+
+	// If number of digits required is more, then 
+	// add 0s at the beginning 
+	while (i < d) 
+		str[i++] = '0'; 
+
+	reverse(str, i); 
+	str[i] = '\0'; 
+	return i; 
 } 
 
-void ftoa(float n, char *res, int afterpoint) 
-{ 
-    // Extract integer part 
-    int ipart = (int)n; 
-  
-    // Extract floating part 
-    float fpart = n - (float)ipart; 
-  
-    // convert integer part to string 
-    int i = intToStr(ipart, res, 0); 
-  
-    // check for display option after point 
-    if (afterpoint != 0) 
-    { 
-        res[i] = '.';  // add dot 
-  
-        // Get the value of fraction part upto given no. 
-        // of points after dot. The third parameter is needed 
-        // to handle cases like 233.007 
-        fpart = fpart * pow(10, afterpoint); 
-  
-        intToStr((int)fpart, res + i + 1, afterpoint); 
-    } 
+void ftoa(float n, char *res, int afterpoint) { 
+// Extract integer part 
+	int ipart = (int)n; 
+
+	// Extract floating part 
+	float fpart = n - (float)ipart; 
+
+	// convert integer part to string 
+	int i = intToStr(ipart, res, 0); 
+
+	// check for display option after point 
+	if (afterpoint != 0) { 
+		res[i] = '.';  // add dot 
+
+		// Get the value of fraction part upto given no. 
+		// of points after dot. The third parameter is needed 
+		// to handle cases like 233.007 
+		fpart = fpart * pow(10, afterpoint); 
+
+		intToStr((int)fpart, res + i + 1, afterpoint); 
+	} 
 } 
